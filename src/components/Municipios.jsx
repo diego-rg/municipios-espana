17b895dfb3334e3af862c6ad6d75e107143687bf
eu <diego_thegarden@hotmail.com>
2022-08-21 19:56:10 +0200

    filtering

--- src/components/Municipios.jsx
+++ src/components/Municipios.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const Municipios = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [codProv, setCodProv] = useState("15");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,7 +21,7 @@ const Municipios = () => {
     };
 
     fetchData();
-  }, []);
+  }, [codProv]);
 
   return (
     <div>
@@ -40,17 +41,19 @@ const Municipios = () => {
               </tr>
             </thead>
             <tbody>
-              {data.municipios.map((municipio) => (
-                <tr key={municipio.CODIGOINE}>
-                  <td>{municipio.NOMBRE}</td>
-                  <td>{municipio.NOMBRE_PROVINCIA}</td>
-                  <td>{municipio.POBLACION_MUNI}</td>
-                  <td>{Math.round(municipio.SUPERFICIE) / 100}</td>
-                  <td>{municipio.ALTITUD}</td>
-                  <td>{municipio.LONGITUD_ETRS89_REGCAN95}</td>
-                  <td>{municipio.LATITUD_ETRS89_REGCAN95}</td>
-                </tr>
-              ))}
+              {data.municipios
+                .filter((municipio) => municipio.CODPROV === codProv)
+                .map((municipio) => (
+                  <tr key={municipio.CODIGOINE}>
+                    <td>{municipio.NOMBRE}</td>
+                    <td>{municipio.NOMBRE_PROVINCIA}</td>
+                    <td>{municipio.POBLACION_MUNI}</td>
+                    <td>{Math.round(municipio.SUPERFICIE) / 100}</td>
+                    <td>{municipio.ALTITUD}</td>
+                    <td>{municipio.LONGITUD_ETRS89_REGCAN95}</td>
+                    <td>{municipio.LATITUD_ETRS89_REGCAN95}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
