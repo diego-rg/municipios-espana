d9d3aa40569bcebe2d6282feff14bfdd80fb0abe
eu <diego_thegarden@hotmail.com>
2022-08-22 22:07:50 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Provincias from "./components/Provincias";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App max-w-screen-xl m-auto">
+    <div className="App max-w-screen-xl m-auto min-h-screen flex flex-col">
       <Header />
       <Provincias />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="p-4 md:p-4 mt-auto">
+      <p className="text-center font-semibold">API: https://www.el-tiempo.net/</p>
+    </div>
+  );
+};
+
+export default Footer;
